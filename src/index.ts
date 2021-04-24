6e83524ca9ffa2ad10c255dafb58b58a8a15e9ef
funlennysub <26184007+funlennysub@users.noreply.github.com>
2021-04-24 18:03:02 +0300

    events

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,17 @@
 import Bot from './Bot'
+import { GuildTextableChannel, Message } from 'eris'
 
 const bot = Bot.initialize()
 bot.start()
+
+// todo: module handler
+bot.client.on('messageCreate', async (msg: Message<GuildTextableChannel>) => {
+  const suggestions = ['760130786963095553']
+
+  if (suggestions.includes(msg.channel.id) && (msg.referencedMessage === null)) {
+    await msg.addReaction('👍')
+    await msg.addReaction('👎')
+  }
+})
 
 export default bot
