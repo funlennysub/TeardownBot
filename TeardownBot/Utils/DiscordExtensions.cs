d3a828b41f157b530e362919dfe85ffad149c93c
funlennysub <26184007+funlennysub@users.noreply.github.com>
2021-06-10 00:51:04 +0300

    tags command port

---
+++ TeardownBot/Utils/DiscordExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using DSharpPlus.Entities;
+
+namespace TeardownBot.Utils
+{
+  public static class DiscordExtensions
+  {
+    public static string FormatDiscordUser(DiscordUser user)
+    {
+      return $"{user.Mention} ({user.Username}#{user.Discriminator})";
+    }
+
+    public static string ClearString(string str)
+    {
+      return Regex.Replace(str, @"/([*_~`>\\])/g", m => $"\\{m.Groups[1].Value}");
+    }
+  }
+}
